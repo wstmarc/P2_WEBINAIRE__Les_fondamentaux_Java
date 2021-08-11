e05d72aa397767b213b64b8fd3e3e8004acfbeb4
alexandre.iwanesko@gmail.com <Akkhavanh31!>
2021-08-11 21:52:54 +0200

    Initial commit

---
+++ WebinarP2/src/Application.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class Application {
+
+    private final String path = ".\\WebinarP2\\src\\resources\\";
+
+    public void readFile() throws IOException {
+        //Remplacer le nom du fichier à lire ici
+        String fileToRead = "longText";
+        String completePath = path + fileToRead;
+
+        //TODO écrire le code pour lire ici
+        //il faut lire le completePath
+        try{
+
+        }catch (){}
+    }
+
+    public static void main(String[] args) throws IOException {
+        Application application = new Application();
+
+        application.readFile();
+    }
+}
